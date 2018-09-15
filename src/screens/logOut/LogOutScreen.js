17fb5e9f70af1e96f2b4145a629e260422b09fe4
Sergei Galkovskii <sergei.galkovskii@Sergeis-MacBook-Pro.local>
2018-09-15 16:42:19 +0300

    Created log out screen

---
+++ src/screens/logOut/LogOutScreen.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Grid, Button, Text, Col } from 'native-base';
+import { withNavigation } from 'react-navigation';
+import WrapperNavigatorItemComponent from '../../components/WrapperNavigatorItem/WrapperNavigatorItemComponent';
+import styles from './styles';
+
+const screenTitle = 'Log Out';
+
+class LogOutScreen extends Component {
+    static navigationOptions = {
+        title: 'Log Out',
+    };
+
+    onClickButton = () => {
+        this.props.navigation.navigate('Login');
+    }
+
+    render() {
+        return (
+            <WrapperNavigatorItemComponent title={screenTitle}>
+                <Grid style={styles.container}>
+                    <Text style={styles.text}>Good Bye</Text>
+                    <Col style={styles.buttonCol}>
+                        <Button primary style={styles.button} onPress={this.onClickButton}>
+                            <Text>Log Out</Text>
+                        </Button>
+                    </Col>
+                </Grid>
+            </WrapperNavigatorItemComponent>
+        );
+    }
+}
+
+LogOutScreen.propTypes = {
+    navigation: PropTypes.object,
+};
+
+export default withNavigation(LogOutScreen);
---
+++ src/screens/logOut/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingVertical: 100,
+    },
+    text: {
+        fontSize: 26,
+    },
+    buttonCol: {
+        paddingVertical: 50,
+    },
+    button: {
+        paddingHorizontal: 15,
+    },
+});
+
+export default styles;
