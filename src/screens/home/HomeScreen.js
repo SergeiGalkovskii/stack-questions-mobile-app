0b15706b5c837735914d880bd20f6c623df55748
Sergei Galkovskii <sergei.galkovskii@Sergeis-MacBook-Pro.local>
2018-09-15 15:00:14 +0300

    Created home screen

---
+++ src/screens/home/HomeScreen.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Container } from 'native-base';
+import WrapperNavigatorItemComponent from '../../components/WrapperNavigatorItem/WrapperNavigatorItemComponent';
+import styles from './styles';
+
+const screenTitle = 'Home';
+
+class HomeScreen extends Component {
+  state = {
+    userName: 'Steve',
+  };
+
+  render() {
+    const textContent = `Hello, ${this.state.userName}!`;
+    return (
+      <WrapperNavigatorItemComponent title={screenTitle}>
+        <Container style={styles.container}>
+          <Text style={styles.text}>{textContent}</Text>
+        </Container>
+      </WrapperNavigatorItemComponent >
+    );
+  }
+}
+
+export default HomeScreen;
---
+++ src/screens/home/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingVertical: 100,
+    },
+    text: {
+        fontSize: 21
+    }
+});
+
+export default styles;
