bca16cf0390531ce00bd9511557d5c2d17d7f7df
Sergei Galkovskii <sergei.galkovskii@Sergeis-MacBook-Pro.local>
2018-09-16 19:34:45 +0300

    Created questions list and styles for it in stackoverflow component

--- src/screens/stackoverflow/StackoverflowScreen.js
+++ src/screens/stackoverflow/StackoverflowScreen.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { Container, Content } from 'native-base';
+import PropTypes from 'prop-types';
+import { Text, FlatList, ActivityIndicator, View } from 'react-native';
+import { Content, Col } from 'native-base';
+import { connect } from 'react-redux';
+import { getStackQuestionsDataAction } from '../../store/actions';
 import WrapperNavigatorItemComponent from '../../components/wrapperNavigatorItem/WrapperNavigatorItemComponent';
+import styles from './styles';
 
+const screenTitle = 'StackOverFlow';
+@connect(
+  state => ({
+    dataState: state.stack,
+  }),
+  dispatch => ({
+    onGetData: () => dispatch(getStackQuestionsDataAction()),
+  })
+)
 class StackoverflowScreen extends Component {
+  componentDidMount() {
+    this.props.onGetData();
+  }
+
+  renderItem = ({ item }) => (
+    <Col style={styles.listItem}>
+      <Text numberOfLines={1}>{item.title}</Text>
+    </Col>
+  );
+
+  renderSeparator = () => (
+    <Col style={styles.listSeparator} size={1} />
+  );
+
   render() {
+    const {
+      dataState,
+    } = this.props;
+
+    if (dataState.isLoading) {
+      return (
+        <View style={styles.containerLoader}>
+          <ActivityIndicator size="small" />
+        </View>);
+    }
     return (
-      <WrapperNavigatorItemComponent title='StackOverFlow'>
+      <WrapperNavigatorItemComponent title={screenTitle}>
         <Content>
-          <Text>Stack</Text>
+          <FlatList
+            data={dataState.data}
+            keyExtractor={(x, i) => i}
+            renderItem={this.renderItem}
+            ItemSeparatorComponent={this.renderSeparator}
+          />
         </Content>
       </WrapperNavigatorItemComponent>
     );
   }
 }
+
+StackoverflowScreen.propTypes = {
+  dataState: PropTypes.object,
+  onGetData: PropTypes.func,
+};
 
 export default StackoverflowScreen;
---
+++ src/screens/stackoverflow/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create(({
+  listItem: {
+    paddingHorizontal: 10,
+    paddingVertical: 18,
+  },
+  listSeparator: {
+    height: 1,
+    backgroundColor: '#CED0CE',
+  },
+  containerLoader: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+}));
+
+export default styles;
